ee0995db82f7fbdaaaf770b8831d1fbff66c4a75
dumi6 <dumi6@outlook.com>
2018-09-11 21:38:58 +0100

    Incomplete code

---
+++ Eclipse_Work/src/dumi/CallingMethods.java
@@ -0,0 +1,13 @@
+package dumi;
+
+public class CallingMethods { 
+	public static void main(String []args) {
+		
+		
+		
+		public static void incorrect();
+		public static void correct();
+		
+	}
+
+}
